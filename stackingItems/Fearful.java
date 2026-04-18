752b5317817a8c0bc33699e99965a95990ebb781
Laura <Laura.parra-v@mail.escuelaing.edu.co>
2026-04-18 16:42:59 -0500

    Update Fearful.java
    Fearful sobreescribe canEnter para no entrar si su taza compañera no está en la torre, y canExit para no salir si está tapando a su taza.

--- stackingItems/Fearful.java
+++ stackingItems/Fearful.java
@@ -1,4 +1,4 @@
-
+import java.util.ArrayList;
 /**
  * La clase Fearful es una subclase de Lid, si su numero de taza no esta en la torre, no se le permitira ingresar a la misma, ademas si llega a tapar su taza de
  * igual numero, no podra ser eliminada, la unica forma de borrarla mientras tapa a su taza es eliminando a su taza.
@@ -7,7 +7,6 @@
  * @version 1.3
  */
 public class Fearful extends Lid {
-
     /**
      * Constructor for objects of class Fearful
      */
@@ -16,12 +15,30 @@ public class Fearful extends Lid {
     }
 
     /**
-     * An example of a method - replace this comment with your own
+     *  Una tapa fearful no puede entrar si su taza compañera no esta en el stack.
      * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * @param stack El stack actual de la torre.
+     * @param number Numero identificador de la tapa.
+     * @return boolean  true si su taza compañera esta en el stack, false en caso contrario.
      */
-    public int sampleMethod(int y) {
-        return 0;
+    @Override
+    public boolean canEnter(ArrayList<Element> stack, int number) {
+        for (Element e : stack) {
+            if (e instanceof Cup && e.getNumber() == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    /**
+     * Una tapa fearful no puede salir si esta tapando a su taza.
+     * 
+     * @param cupAndLidPositions Arreglo con las posiciones de la taza y tapa de igual numero.
+     * @return boolean - false si esta tapando a su taza, true en caso contrario.
+     */
+    @Override
+    public boolean canExit(int[] cupAndLidPositions) {
+        return cupAndLidPositions[2] != 1;
     }
 }
