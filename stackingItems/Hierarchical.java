173a0bbf5adb36dcb0017999ae73e139d207036f
Laura <Laura.parra-v@mail.escuelaing.edu.co>
2026-04-18 16:46:37 -0500

    Update Hierarchical.java
    Hierarchical sobreescribe canBeRemoved para no poder ser removida si está en el fondo de la torre, y onPush para desplazar hacia arriba los elementos de menor tamaño al entrar.

--- stackingItems/Hierarchical.java
+++ stackingItems/Hierarchical.java
@@ -1,4 +1,4 @@
-
+import java.util.ArrayList;
 /**
  * La clase Hierarchical es una subclase de Cup, si el elemento debajo de esta taza, es de menor tamaño que ella, la pone encima suyo, si llega a tocar el fondo
  * de la torre, no se deja mover, pero se puede eliminar en cualquier momento.
@@ -7,22 +7,47 @@
  * @version 1.3
  */
 public class Hierarchical extends Cup {
-
     /**
      * Constructor for objects of class Hierarchical
+     * @param number Numero identificador de la taza.
      */
     public Hierarchical(int number) {
         super(number);
     }
 
     /**
-     * An example of a method - replace this comment with your own
+     * Una taza hierarchical no se puede quitar si esta en el fondo de la torre
      * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * @param positionInStack Posicion de la taza en el stack.
+     * @return boolean - false si esta en el fondo, true en cualquier otro caso. 
      */
-    public int sampleMethod(int y)
-    {
-        return 0;
+    @Override
+    public boolean canBeRemoved (int positionInStack){
+        return positionInStack !=0;
+    }
+    
+    /**
+     * Al entrar a la torre, dezplaza hacia arriba todos los elementos de menor tamaño que ella
+     * 
+     * @param stack El stack actual de la torre
+     */
+    @Override
+    public void onPush (ArrayList<Element> stack){
+        int myWidth = this.getWidth();
+        // Recolecta los elementos mas pequelos que estan debajo
+        ArrayList <Element> toMove = new ArrayList <Element>();
+        for (int i = stack.size() -2; i >= 0; i--){
+            Element e = stack.get(i);
+            if(e.getWidth() < myWidth){
+                toMove.add (0,e);
+                stack.remove (i);
+            } else {
+                break;
+            }
+        }
+        
+        for(Element e : toMove){
+            stack.add(e);
+        }
     }
 }
