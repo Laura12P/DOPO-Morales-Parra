6f5ddb86ee1fee2a795fa577aa193fa6192d8a01
Laura <Laura.parra-v@mail.escuelaing.edu.co>
2026-05-08 10:38:54 -0500

    Update CollisionController.java

--- TheWorldsHardestGame/src/domain/CollisionController.java
+++ TheWorldsHardestGame/src/domain/CollisionController.java
@@ -1,15 +1,44 @@
 package domain;
 
+import java.util.ArrayList;
+
 /* La clase CollisionController es la encargada de calcular y determinar las colisiones entre elementos del tablero.
- * 
+ *
  * @author Laura Juliana Parra Velandia y Daniel Santiago Morales Perdomo
  */
- 
+
 public class CollisionController {
-	
-	/*Constructor de la clase CollisionController
-	 */
-	public CollisionController() {
-		
-	}
+
+    public CollisionController() {
+    }
+
+    public void checkCollisions(ArrayList<Player> players, ArrayList<Enemy> enemies,
+                                 ArrayList<Collectionable> collectables, EndZone endZone) {
+        for (Player player : players) {
+            // Colisión con enemigos
+            for (Enemy enemy : enemies) {
+                if (intersects(player, enemy)) {
+                    player.respawn(player.positionX, player.positionY);
+                }
+            }
+            // Colisión con monedas
+            for (Collectionable c : collectables) {
+                if (!c.isCollected() && intersects(player, c)) {
+                    c.collect();
+                    player.coinCollected();
+                }
+            }
+            // Colisión con zona final
+            if (intersects(player, endZone)) {
+                // TODO: notificar que el nivel terminó
+            }
+        }
+    }
+
+    private boolean intersects(Element a, Element b) {
+        return a.positionX < b.positionX + b.width &&
+               a.positionX + a.width > b.positionX &&
+               a.positionY < b.positionY + b.height &&
+               a.positionY + a.height > b.positionY;
+    }
 }
