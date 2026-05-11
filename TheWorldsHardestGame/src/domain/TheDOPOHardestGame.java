1079002afcbf077a3b4b32953eed0bbb17ca0bcf
Laura <Laura.parra-v@mail.escuelaing.edu.co>
2026-05-10 21:57:15 -0500

    Update TheDOPOHardestGame.java

--- TheWorldsHardestGame/src/domain/TheDOPOHardestGame.java
+++ TheWorldsHardestGame/src/domain/TheDOPOHardestGame.java
@@ -1,12 +1,14 @@
 package domain;
 
+import java.io.IOException;
+
 /* La clase TheDOPOHardestGame es la clase principal en el modelo, encargada de gestionar el tablero y sus acciones.
  * 
  * @author Laura Juliana Parra Velandia y Daniel Santiago Morales Perdomo
  */
 
 public class TheDOPOHardestGame {
-	private Board board;
+	public Board board;
 	
 	/*Constructor de la clase TheDOPOHardestGame
 	 * 
@@ -15,8 +17,8 @@ public class TheDOPOHardestGame {
 	 * @param startZone Primera zona de respawn donde comienza el jugador.
 	 * @param endZona Zona donde se termina el juego si el jugador colisiona con esta.
 	 */
-	public TheDOPOHardestGame (int ancho, int alto, StartZone startZone, EndZone endZone) {
-		board = new Board(ancho, alto, startZone, endZone);
-	}
+	public TheDOPOHardestGame(String levelPath) throws IOException {
+        board = LevelLoader.loadFromFile(levelPath);
+    }
 	
 }
